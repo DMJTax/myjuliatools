97575291cd5fa0b99daf77aa818ecf4b449ece2c
David Tax <dtax@davids-mbp.ws.tudelft.net>
2024-11-26 14:18:40 +0100

    Added min-max scaling, better help.

--- scaling.jl
+++ scaling.jl
@@ -1,7 +1,12 @@
 
 export scalem
 
-# Make a scale mapping
+"""
+      w = scalem(a,stype="unitvar",smallval=1e-9)
+Scale the features in dataset `a`. For now, the following scalings are defined:
+   `unitvar`     set mean to 0, and variance to 1
+   `minmax`      set the minimum to 0 and the maximum to 1
+"""
 function scalem(stype="unitvar",smallval=1e-9)
     params = Dict{String,Any}("stype"=>stype,"eps"=>smallval)
     return Prmapping("Scale map","untrained",fitScalem!,predictScalem,params,nothing)
@@ -13,10 +18,13 @@ function fitScalem!(w,a)
     # unpack
     stype = w.data["stype"]
     # do it
+    X = a.data
     if (stype=="unitvar")
-        X = a.data
         w.data["mean"] = mean(X,dims=1)
         w.data["scale"] = std(X,dims=1) .+ w.data["eps"]
+    elseif (stype=="minmax")
+        w.data["min"] = minimum(X,dims=1)
+        w.data["scale"] = maximum(X,dims=1) .- w.data["min"]
     else
         error("This scaling is not known")
     end
@@ -30,7 +38,10 @@ function predictScalem(w,a)
     # do it
     if (stype=="unitvar")
         X = (a.data .-  w.data["mean"]) ./ w.data["scale"]
+    elseif (stype=="minmax")
+        X = (a.data .- w.data["min"]) ./ w.data["scale"]
     end
+    # store
     out = deepcopy(a)
     out.data = X
     return out
