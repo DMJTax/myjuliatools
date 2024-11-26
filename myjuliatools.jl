24ec32f63c73e6eeed2ad0314e52aef75310f7e6
David Tax <dtax@davids-mbp.ws.tudelft.net>
2024-11-26 14:15:17 +0100

    Better help

--- myjuliatools.jl
+++ myjuliatools.jl
@@ -1,16 +1,23 @@
 module myjuliatools
 
+include("prtools.jl")
 include("prdataset.jl")
 include("prmapping.jl")
 include("linearr.jl")
-include("mydatasets.jl")
+include("regression.jl")
 include("generative.jl")
-include("datadescription.jl")
-include("crossvalidation.jl")
-include("mahaldist.jl")
+include("nmc.jl")
 include("lda.jl")
-include("pca.jl")
+include("crossvalidation.jl")
 include("mog.jl")
+include("logistic.jl")
+include("perceptron.jl")
+include("mclassc.jl")
+include("pca.jl")
 include("scaling.jl")
+include("mydatasets.jl")
+
+include("datadescription.jl")
+include("mahaldist.jl")
 
 end # end of module
